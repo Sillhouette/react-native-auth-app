4c8e2e4415584bfed08d7fbdf48e1dae4d1cac54
github username <raustin.melchior@gmail.com>
2019-04-30 17:10:56 -0700

    add email and password input fields

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
   state = {
-    text: ''
+    email: '',
+    password: ''
   };
 
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput
-            value={this.state.text}
-            style={{ height: 20, width: 100 }}
-            onChangeText={text => this.setState({ text })}
+          <Input
+            label="Email: "
+            placeholder="example@domain.com"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
 
-        <CardSection />
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password: "
+            placeholder="password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
 
         <CardSection>
           <Button>Log in</Button>
